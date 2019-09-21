794dd2a317a9b1cde80c385e02da85cb1b30d30f
Flavien CADET <flavien.cadet@nocturlab.fr>
2019-09-21 22:07:56 +0200

    Send tar and not only files

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,13 +7,19 @@ if [[ -z "$DISCORD_WEBHOOK" ]]; then
 fi
 
 # Ensure that the file path is present
+if [[ -z "$ARCHIVE_NAME" ]]; then
+  echo "You must set the ARCHIVE_NAME env variable to define the name of the archive. (Without extension ex: 'release' and not 'release.tar.gz')"
+  exit 1
+fi
+
+# Ensure that the file path is present
 if [[ -z "$1" ]]; then
   echo "You must pass at least one argument to this action, the path to the file to upload."
   exit 1
 fi
 
-for f in $1; do
-  curl -H 'Content-Type: multipart/form-data' \
+tar -czvf ${ARCHIVE_NAME}.tar.gz $1
+
+curl -H 'Content-Type: multipart/form-data' \
   -X POST \
-  -F "file=@$f" $DISCORD_WEBHOOK
-done
+  -F "file=@${ARCHIVE_NAME}.tar.gz" $DISCORD_WEBHOOK
